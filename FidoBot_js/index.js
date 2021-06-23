907e21f058d3f3bde1b47c3b1fdc7f49ae2648eb
arifire21 <ariwolfpup@icloud.com>
2021-06-23 18:11:00 -0400

    first working test command! (will be moved)

--- FidoBot_js/index.js
+++ FidoBot_js/index.js
@@ -15,24 +15,49 @@ for (const file of commandFiles) {
 
 client.once('ready', () => {
 	console.log('FidoBot-- js version! Ready!');
+	client.user.setPresence({ activity: { name: 'Companion to the Follow Fido app!' }, status: 'online' });
+	
+	//test for single server, not global
+	client.api.applications(client.user.id).guilds(config.guild1).commands.post({
+		data: {
+			name: 'bork',
+			description: "slash cmd--reply w a message"
+		}
+	});
+
+	client.ws.on('INTERACTION_CREATE', async interaction => {
+		const command = interaction.data.name.toLowerCase();
+		const args = interaction.data.options;
+
+		if(command == 'bork'){
+			client.api.interactions(interaction.id, interaction.token).callback.post({
+				data: {
+					type: 4,
+					data: {
+						content: "First slash command!"
+					}
+				}
+			});
+		}
+	});
 });
 
 
-client.on('message', message => {
-	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+// client.on('message', message => {
+// 	if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+// 	const args = message.content.slice(config.prefix.length).trim().split(/ +/);
+// 	const command = args.shift().toLowerCase();
 
-	if (!client.commands.has(command)) return;
+// 	if (!client.commands.has(command)) return;
 
-	try {
-		client.commands.get(command).execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
-});
+// 	try {
+// 		client.commands.get(command).execute(message, args);
+// 	} catch (error) {
+// 		console.error(error);
+// 		message.reply('there was an error trying to execute that command!');
+// 	}
+// });
 
 
 client.login(config.token);

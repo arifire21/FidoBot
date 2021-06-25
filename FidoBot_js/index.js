b338ba373a73950728ddee03e62e65466a075aae
arifire21 <ariwolfpup@icloud.com>
2021-06-24 22:43:45 -0400

    test command for deffered responses, works

--- FidoBot_js/index.js
+++ FidoBot_js/index.js
@@ -3,6 +3,7 @@ const config = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+const axios = require('axios');
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -18,10 +19,14 @@ client.once('ready', () => {
 	client.user.setPresence({ activity: { name: 'Companion to the Follow Fido app!' }, status: 'online' });
 	
 	//test for global cmd
-	client.api.applications(client.user.id).commands.post({
+	client.api.applications(client.user.id).guilds(config.guild1).commands.post({
 		data: {
 			name: 'bork',
 			description: "reply w a message"
+		},
+		data: {
+			name: 'typetest',
+			description: "test typing duration"
 		}
 	});
 
@@ -39,8 +44,34 @@ client.once('ready', () => {
 				}
 			});
 		}
+
+		if(command == 'typetest'){
+			// client
+			client.api.interactions(interaction.id, interaction.token).callback.post({
+				data: {
+					type: 5,
+				}
+			});
+
+			axios.patch(`https://discord.com/api/v8/webhooks/${config.appid}/${interaction.token}/messages/@original`, { content: 'New content' });
+
+		}
 	});
 });
+
+// const editInteraction = async (client, interaction, response) => {
+//     // Set the data as embed if reponse is an embed object else as content
+//     const data = typeof response === 'object' ? { embeds: [ response ] } : { content: response };
+//     // Get the channel object by channel id:
+//     const channel = await client.channels.resolve(interaction.channel_id);
+//     // Edit the original interaction response:
+//     return axios
+//         .patch(`https://discord.com/api/v8/webhooks/${appId}/${interaction.token}/messages/@original`, data)
+//         .then((answer) => {
+//             // Return the message object:
+//             return channel.messages.fetch(answer.data.id)
+//         })
+// };
 
 //for non-slash commands
 client.on('message', message => {

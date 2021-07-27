ea317511dc1ebac813ce64b0fa3af212a618c830
arifire21 <ariwolfpup@icloud.com>
2021-07-27 19:22:50 -0400

    updated embed functionality

--- FidoBot_js/commands/embed-test.js
+++ FidoBot_js/commands/embed-test.js
@@ -1,34 +1,45 @@
 const Discord = require('discord.js');
 // const index = require('./../index.js');
-let dogNameArg = '';
+var dogNameArg = '';
+var dogBreed = '';
 
 const dogProfile = new Discord.MessageEmbed()
     //todo ping user when embed is shown
     .setColor('#45C3D6')
-    .setTitle(dogNameArg)
+    // .setTitle(dogNameArg)
+    .setTitle('Dog Name')
     .setAuthor('Follow Fido App')
     //commented out to avoid taking up resources, will replace with actual user acc stuff    
     // .setThumbnail('https://raw.githubusercontent.com/arifire21/FidoBot/main/FidoBot_js/dog_icons/dog1.png')
-    .addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	);
+    // .addFields(
+	// 	{ name: 'Breed', value: 'Some value here' },
+	// 	{ name: '\u200B', value: '\u200B' },
+	// 	{ name: 'Today\'s Walk Dist.', value: 'Some value here', inline: true },
+	// 	{ name: 'Inline field title', value: 'Some value here', inline: true },
+    // )
+	;
 
     module.exports = {
     name: 'embedtest',
 	description: 'test',
 	execute(message, args) {
         const user = message.author;
-        console.log(args[0]);
         dogNameArg = args[0];
+        dogBreed = args[1];
+        // console.log(dogNameArg);
 
         if(!dogNameArg){
             message.reply('you need to provide a single dog name.');
         }
         else{
             message.channel.send(`<@${user.id}> Profile for your dog \`` + dogNameArg + `\``);
+            dogProfile.setTitle(dogNameArg);
+            dogProfile.addFields(
+                { name: 'Breed', value: dogBreed },
+                { name: '\u200B', value: '\u200B' },
+                { name: 'Today\'s Walk Dist.', value: 'Some value here', inline: true },
+                { name: 'Inline field title', value: 'Some value here', inline: true },
+            )
             message.channel.send(dogProfile);
         }
     },

e13b9793445f993ab01e8194d7523220f5baf8a5
arifire21 <ariwolfpup@icloud.com>
2021-08-06 10:55:44 -0400

    temp setup for now-required Intents

--- FidoBot_js/index.js
+++ FidoBot_js/index.js
@@ -2,18 +2,19 @@ const fs = require('fs');
 const axios = require('axios'); //for updating "listening" message
 const config = require('./config.json');
 const testVisibility = require('./follow-fido integration/test_visibility');
-const Discord = require('discord.js');
-const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
-client.commands = new Discord.Collection();
+const {Client, Intents} = require('discord.js');
+// const client = new Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]}, {intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ]});
+const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES ]});
+// client.commands = new Client.Collection();
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
-	client.commands.set(command.name, command);
-}
+// for (const file of commandFiles) {
+// 	const command = require(`./commands/${file}`);
+// 	// set a new item in the Collection
+// 	// with the key as the command name and the value as the exported module
+// 	client.commands.set(command.name, command);
+// }
 
 client.once('ready', () => {
 	console.log('FidoBot-- js version! Ready!');

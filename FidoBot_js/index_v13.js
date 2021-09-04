677c28458de3e2bcf7eee2f869310948a6480aac
arifire21 <ariwolfpup@icloud.com>
2021-09-04 19:34:47 -0400

    discord.js v13 for real this time!! Basic setup

---
+++ FidoBot_js/index_v13.js
@@ -0,0 +1,37 @@
+const config = require('./config.json');
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.commands = new Collection();
+
+const commandFiles = fs.readdirSync('./v13_commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./v13_commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+client.once('ready', () => {
+	console.log('FidoBot-- v13! Ready!');
+	client.user.setPresence({ activity: { name: 'Now js v13!' }, status: 'online' });
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.login(config.token);
